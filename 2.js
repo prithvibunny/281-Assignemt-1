a27d4421ac7107ae74851551a3a2962baf395ede
Prithvi Raj <58871002+prithvibunny@users.noreply.github.com>
2022-02-21 11:40:22 -0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+function download(link) {
+  var element = document.createElement('a');
+  element.setAttribute('href', link);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
